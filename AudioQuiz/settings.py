2983072765ae101d031daf2376e65b192341b824
GU1NHO <thiagorosacarvalho@usp.br>
2024-11-29 19:43:26 -0300

    AudioQuiz 1.1.0 Render Docker 11

--- AudioQuiz/settings.py
+++ AudioQuiz/settings.py
@@ -142,7 +142,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'classes/index'
+LOGIN_REDIRECT_URL = 'classes:index'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
